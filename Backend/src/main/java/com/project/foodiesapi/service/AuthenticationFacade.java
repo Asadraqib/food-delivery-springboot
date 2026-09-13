2fde8d9ecbaa05f9f6cde5015d7f0c8925d5901a
Asad_Raqib <101248375+Asadraqib@users.noreply.github.com>
2026-09-13 14:58:38 +0530

    deleted old backend

--- Backend/src/main/java/com/project/foodiesapi/service/AuthenticationFacade.java
+++
@@ -1,7 +0,0 @@
-package com.project.foodiesapi.service;
-
-import org.springframework.security.core.Authentication;
-
-public interface AuthenticationFacade {
-    Authentication getAuthentication();
-}
