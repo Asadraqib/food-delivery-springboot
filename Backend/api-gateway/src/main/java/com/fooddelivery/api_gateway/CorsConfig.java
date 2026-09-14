2336ae200150702710f957fc6d30fe463b2dfe47
Asadraqib <asadraqib1786@gmail.com>
2026-09-14 20:17:00 +0530

    fix: correct Netlify deployment URL in CORS configuration

--- Backend/api-gateway/src/main/java/com/fooddelivery/api_gateway/CorsConfig.java
+++ Backend/api-gateway/src/main/java/com/fooddelivery/api_gateway/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173","https://food-delivery-microservices.netlify.app")
+                        .allowedOrigins("http://localhost:5173","https://food-delivery-microser.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
