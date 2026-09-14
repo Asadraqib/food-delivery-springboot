bdd4212dbd5d9844f3f914e89e76fed172ce4371
Asadraqib <asadraqib1786@gmail.com>
2026-09-14 18:14:03 +0530

    fix: update API base URL to use HTTPS for improved security

--- Frontend/food-delivery-frontend/src/api/axios.js
+++ Frontend/food-delivery-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8888",
+  baseURL: "https://api-gateway-nul6.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
